7bdf6d7c1e8d4cd519ebb6635e1f03e2f18530eb
Henry <18618415652@163.com>
2017-12-31 14:29:46 +0800

    更新一下

--- js/app.js
+++ js/app.js
@@ -1,6 +1,50 @@
 (function (window) {
 	'use strict';
+	var todos = [{
+			text: '吃饭',
+			completed: true
+		},
+		{
+			text: '睡觉',
+			completed: false
+		},
+		{
+			text: '吃饭',
+			completed: true
+		},
 
-	// Your starting point. Enjoy the ride!
+	]
+	new Vue({
+		el: '#app',
+		data: {
+			title: '美香时钟',
+			todos: todos,
+			text: ''
+
+		},
+		methods: {
+			add() {
+				if (this.text.trim().length === 0) {
+					return
+				}
+				this.todos.push({
+					text: this.text,
+					completed: false
+				})
+				this.text = ''
+			},
+			destory:function(text){
+				let todoIndex
+				this.todos.find((todo,index)=>{
+					if(todo.text===text){
+						todoIndex=index
+						
+					}
+				})
+				this.todos.splice(todoIndex,1)
+			},
+		
+		}
+	})
 
 })(window);
